438457c02ca7d4a60c38b9043be6399aa9f267c0
xy198781 <xy198781@sina.com>
2014-01-28 21:15:07 +0800

    delete backup files.

--- src/org/oeleo/myfirstandroid/MainActivity.java
+++ src/org/oeleo/myfirstandroid/MainActivity.java
@@ -20,49 +20,4 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-    	MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                openSearch();
-                return true;
-            case R.id.action_settings:
-                openSettings();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    
-    private void openSettings() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	private void openSearch() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	/** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
 }
